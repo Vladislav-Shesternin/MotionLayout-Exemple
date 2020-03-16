73be12a6ff87b583ee784e48f3de350980fab4ac
Vlad-Shesternin <valdon1202@gmail.com>
2020-03-16 14:07:46 +0200

    2

--- app/src/main/java/com/example/motionlayout_example/MainActivity.java
+++ app/src/main/java/com/example/motionlayout_example/MainActivity.java
@@ -10,7 +10,9 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button
             mBtn1, mBtn2, mBtn3,
-            mBtn4, mBtn5, mBtn6;
+            mBtn4, mBtn5, mBtn6,
+            mBtn7, mBtn8, mBtn9,
+            mBtn10, mBtn11, mBtn12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBtn4 = findViewById(R.id.btn_04);
         mBtn5 = findViewById(R.id.btn_05);
         mBtn6 = findViewById(R.id.btn_06);
+        mBtn7 = findViewById(R.id.btn_07);
+        mBtn8 = findViewById(R.id.btn_08);
+        mBtn9 = findViewById(R.id.btn_09);
+        mBtn10 = findViewById(R.id.btn_10);
+        mBtn11 = findViewById(R.id.btn_11);
+        mBtn12 = findViewById(R.id.btn_12);
 
         mBtn1.setOnClickListener(this);
         mBtn2.setOnClickListener(this);
@@ -29,6 +37,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBtn4.setOnClickListener(this);
         mBtn5.setOnClickListener(this);
         mBtn6.setOnClickListener(this);
+        mBtn7.setOnClickListener(this);
+        mBtn8.setOnClickListener(this);
+        mBtn9.setOnClickListener(this);
+        mBtn10.setOnClickListener(this);
+        mBtn11.setOnClickListener(this);
+        mBtn12.setOnClickListener(this);
     }
 
     @Override
@@ -52,6 +66,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.btn_06:
                 intent = new Intent(this, Motion_06.class);
+                break;
+            case R.id.btn_07:
+                intent = new Intent(this, Motion_07.class);
+                break;
+            case R.id.btn_08:
+                intent = new Intent(this, Motion_08.class);
+                break;
+            case R.id.btn_09:
+                intent = new Intent(this, Motion_09.class);
+                break;
+            case R.id.btn_10:
+                intent = new Intent(this, Motion_10.class);
+                break;
+            case R.id.btn_11:
+                intent = new Intent(this, Motion_11.class);
+                break;
+            case R.id.btn_12:
+                intent = new Intent(this, Motion_12.class);
                 break;
         }
         startActivity(intent);
---
+++ app/src/main/java/com/example/motionlayout_example/Motion_07.java
@@ -0,0 +1,17 @@
+package com.example.motionlayout_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Motion_07 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.motion_07);
+
+    }
+}
