ecd4e7c8a8e08ccf2de27de1ae5983b57168f682
Vlad-Shesternin <valdon1202@gmail.com>
2020-03-21 13:34:31 +0200

    4

--- app/src/main/java/com/example/motionlayout_example/Motion_07.java
+++ app/src/main/java/com/example/motionlayout_example/Motion_07.java
@@ -1,17 +1,46 @@
 package com.example.motionlayout_example;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Motion_07 extends AppCompatActivity {
+public class Motion_07 extends AppCompatActivity implements View.OnClickListener {
+
+    MotionLayout motionLayout_07;
+    EditText edit_1, edit_2;
+    Button btn_1;
+
+    int set1, set2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.motion_07);
 
+        motionLayout_07 = findViewById(R.id.motion_layout_07);
+        edit_1 = findViewById(R.id.edit_07_1);
+        edit_2 = findViewById(R.id.edit_07_2);
+        btn_1 = findViewById(R.id.btn_1);
+
+        set1 = R.id.no_07;
+        set2 = R.id.yes_07;
+
+        btn_1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String text = edit_2.getText().toString();
+        if (text.equals("0123456789")) {
+            motionLayout_07.setTransition(set2, set2);
+            motionLayout_07.transitionToEnd();
+        } else {
+            motionLayout_07.setTransition(set1, set1);
+            motionLayout_07.transitionToEnd();
+        }
     }
 }
--- app/src/main/java/com/example/motionlayout_example/Motion_15.java
+++ app/src/main/java/com/example/motionlayout_example/Motion_15.java
@@ -2,56 +2,47 @@ package com.example.motionlayout_example;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.motion.widget.MotionLayout;
-import androidx.constraintlayout.motion.widget.MotionScene;
-import androidx.constraintlayout.motion.widget.TransitionAdapter;
+import androidx.lifecycle.Lifecycle;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-public class Motion_15 extends AppCompatActivity implements MotionLayout.TransitionListener {
+public class Motion_15 extends AppCompatActivity {
 
     Button btn_15_1;
     MotionLayout motionLayout_15;
+    int set1, set2, set3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.motion_15);
 
+        set1 = R.id.set1;
+        set2 = R.id.set2;
+        set3 = R.id.set3;
+
         btn_15_1 = findViewById(R.id.btn_15_1);
         motionLayout_15 = findViewById(R.id.motionLayout_15);
-//Назначаем слушатель для motionLayout_15
-        motionLayout_15.setTransitionListener(this);
-//Чтоби не implementing OnClickListener будет использоватся метод с ОБЕЗАТЕЛЬНЫМИ аргументами (View view)
+
+//TODO Чтоби не implementing OnClickListener будет использоватся метод с ОБЕЗАТЕЛЬНЫМИ аргументами (View view)
         btn_15_1.setOnClickListener(this::start);
     }
 
     public void start(View v) {
-        motionLayout_15.transitionToEnd();
-    }
-
-    @Override
-    public void onTransitionStarted(MotionLayout motionLayout, int startId, int endId) {
-
-    }
-
-    //TODO hello
-    @Override
-    public void onTransitionChange(MotionLayout motionLayout, int startId, int endId, float progress) {
-        System.err.println("startId = " + startId + "endId = " + endId + "progress = " + progress);
-    }
-
-    @Override
-    public void onTransitionCompleted(MotionLayout motionLayout, int currentId) {
-        if (currentId == R.id.end) {
-            motionLayout_15.transitionToStart();
+        if (motionLayout_15.getCurrentState() == set1) {
+            motionLayout_15.setTransition(set1, set2);
+            motionLayout_15.setTransitionDuration(3000);
+            motionLayout_15.transitionToEnd();
+        } else if (motionLayout_15.getCurrentState() == set2) {
+            motionLayout_15.transitionToState(set3);
+            motionLayout_15.setTransitionDuration(3000);
+            motionLayout_15.transitionToEnd();
+        } else {
+            motionLayout_15.setTransition(set3, set1);
+            motionLayout_15.setTransitionDuration(3000);
+            motionLayout_15.transitionToEnd();
         }
-    }
-
-    @Override
-    public void onTransitionTrigger(MotionLayout motionLayout, int triggerId, boolean positive, float progress) {
-
     }
 }
